65fc9c82bd3517ef21078b391f7f9e79120ba331
Aayusha <ayushaghimireg@gmail.com>
2026-03-24 10:05:02 +0545

    Added responsiveness in animated text

--- src/Components/AnimatedText.jsx
+++ src/Components/AnimatedText.jsx
@@ -1,19 +1,48 @@
+// import React from 'react'
+
+// function AnimatedText() {
+//   return (
+//     <>
+//     <div className='bg-[url("../../public/marquree-bg.png")] mt-[10vw]'>
+//         <div className='flex justify-between items-center py-[2.48vw] '>
+//             <img src="../../public/marquee-star.png" alt="star" />
+//             <p className='font-normal text-[1.5rem]'>Website Development</p>
+//             <img src="../../public/marquee-star.png" alt="star" />
+//             <p className='font-normal text-[1.5rem]'>Software Development</p>
+//             <img src="../../public/marquee-star.png" alt="star" />
+//             <p className='font-normal text-[1.5rem]'>SEO Agency</p>
+//             <img src="../../public/marquee-star.png" alt="star" />
+//             <p className='font-normal text-[1.5rem]'>Digital Marketing</p>
+//             <img src="../../public/marquee-star.png" alt="star" />
+//         </div>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default AnimatedText
+
+
 import React from 'react'
 
 function AnimatedText() {
   return (
     <>
     <div className='bg-[url("../../public/marquree-bg.png")] mt-[10vw]'>
-        <div className='flex justify-between items-center py-[2.48vw] '>
-            <img src="../../public/marquee-star.png" alt="star" />
-            <p className='font-normal text-[1.5rem]'>Website Development</p>
-            <img src="../../public/marquee-star.png" alt="star" />
-            <p className='font-normal text-[1.5rem]'>Software Development</p>
-            <img src="../../public/marquee-star.png" alt="star" />
-            <p className='font-normal text-[1.5rem]'>SEO Agency</p>
-            <img src="../../public/marquee-star.png" alt="star" />
-            <p className='font-normal text-[1.5rem]'>Digital Marketing</p>
-            <img src="../../public/marquee-star.png" alt="star" />
+        <div className='flex justify-between items-center py-[2.48vw] px-2'>
+            <img src="../../public/marquee-star.png" alt="star" className='w-[3vw] min-w-[15px]' />
+            <p className='font-normal text-[2vw] md:text-[1.5rem] whitespace-nowrap'>Website Development</p>
+            
+            <img src="../../public/marquee-star.png" alt="star" className='w-[3vw] min-w-[15px]' />
+            <p className='font-normal text-[2vw] md:text-[1.5rem] whitespace-nowrap'>Software Development</p>
+            
+            <img src="../../public/marquee-star.png" alt="star" className='w-[3vw] min-w-[15px]' />
+            <p className='font-normal text-[2vw] md:text-[1.5rem] whitespace-nowrap'>SEO Agency</p>
+            
+            <img src="../../public/marquee-star.png" alt="star" className='w-[3vw] min-w-[15px]' />
+            <p className='font-normal text-[2vw] md:text-[1.5rem] whitespace-nowrap'>Digital Marketing</p>
+            
+            <img src="../../public/marquee-star.png" alt="star" className='w-[3vw] min-w-[15px]' />
         </div>
     </div>
     </>
